c1f7c822644041be7c80646f896f9e7ef825e3b8
santiagopardal <santiagopardal@outlook.com>
2021-10-15 21:02:16 -0300

    Descargar cupon de pago funciona, falta sacar el precio de cada plan

---
+++ obrassociales-com1/resources/views/pdfview.blade.php
@@ -0,0 +1,7 @@
+<table>
+    @foreach(array_keys($costos) as $cliente)
+        <p>Costo de {{ $cliente }}: ${{ $costos[$cliente] }}</p>
+    @endforeach
+    <p>Codigo de pago {{ $codigo }}</p>
+    <h1>Cupon de pago</h1>
+</table>
--- obrassociales-com1/vendor/composer/InstalledVersions.php
+++ obrassociales-com1/vendor/composer/InstalledVersions.php
@@ -30,7 +30,7 @@ private static $installed = array (
     'aliases' => 
     array (
     ),
-    'reference' => '1a7c231fbc45c4936a4ab83191c9084c66d034d0',
+    'reference' => '81376cdf1a621afc00be3e80c35851217e018098',
     'name' => 'laravel/laravel',
   ),
   'versions' => 
@@ -43,6 +43,15 @@ private static $installed = array (
       array (
       ),
       'reference' => '9cb795bf30988e8c96dd3c40623c48a877bc6714',
+    ),
+    'barryvdh/laravel-dompdf' => 
+    array (
+      'pretty_version' => 'v0.9.0',
+      'version' => '0.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5b99e1f94157d74e450f4c97e8444fcaffa2144b',
     ),
     'brick/math' => 
     array (
@@ -102,6 +111,15 @@ private static $installed = array (
       array (
       ),
       'reference' => 'e864bbf5904cb8f5bb334f99209b48018522f042',
+    ),
+    'dompdf/dompdf' => 
+    array (
+      'pretty_version' => 'v1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8768448244967a46d6e67b891d30878e0e15d25c',
     ),
     'dragonmantank/cron-expression' => 
     array (
@@ -469,7 +487,7 @@ private static $installed = array (
       'aliases' => 
       array (
       ),
-      'reference' => '1a7c231fbc45c4936a4ab83191c9084c66d034d0',
+      'reference' => '81376cdf1a621afc00be3e80c35851217e018098',
     ),
     'laravel/sail' => 
     array (
@@ -661,6 +679,24 @@ private static $installed = array (
       array (
       ),
       'reference' => 'bae7c545bef187884426f042434e561ab1ddb182',
+    ),
+    'phenx/php-font-lib' => 
+    array (
+      'pretty_version' => '0.5.2',
+      'version' => '0.5.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ca6ad461f032145fff5971b5985e5af9e7fa88d8',
+    ),
+    'phenx/php-svg-lib' => 
+    array (
+      'pretty_version' => 'v0.3.3',
+      'version' => '0.3.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5fa61b65e612ce1ae15f69b3d223cb14ecc60e32',
     ),
     'phpdocumentor/reflection-common' => 
     array (
@@ -916,6 +952,15 @@ private static $installed = array (
       array (
         0 => '4.2.3',
       ),
+    ),
+    'sabberworm/php-css-parser' => 
+    array (
+      'pretty_version' => '8.3.1',
+      'version' => '8.3.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd217848e1396ef962fb1997cf3e2421acba7f796',
     ),
     'sebastian/cli-parser' => 
     array (
