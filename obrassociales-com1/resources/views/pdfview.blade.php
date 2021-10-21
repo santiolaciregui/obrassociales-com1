ab6f3023b400a5c6dfddee9cbe8a8b3f3cec4aef
santiolaciregui <54482877+santiolaciregui@users.noreply.github.com>
2021-10-21 19:36:02 -0300

    parte vieja del proyecto

--- obrassociales-com1/resources/views/pdfview.blade.php
+++
@@ -1,8 +0,0 @@
-<table>
-    <p>Total: ${{ $total  }}</p>
-    @foreach(array_keys($costos) as $cliente)
-        <p>Costo de {{ $cliente }}: ${{ $costos[$cliente] }}</p>
-    @endforeach
-    <p>Codigo de pago {{ $codigo }}</p>
-    <h1>Cupon de pago</h1>
-</table>
