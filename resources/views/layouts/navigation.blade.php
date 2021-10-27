6cac55c418c757ce9efc08f6440efa718f97ce7c
mayeragustin99 <mayeragustin99@gmail.com>
2021-10-27 19:22:49 -0300

    Nombre de usuario registrado en Navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,3 +1,10 @@
+<html>
+  <head>
+    
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+  </head>
+  <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <ul class="navbar-nav mr-auto">
     <li class="nav-item active">
@@ -8,15 +15,22 @@
       <a class="nav-link" href="/login">Log in</a>
     </li>
     @else
-    <li class="nav-item">
-      <form method="POST" action="{{ route('logout') }}">
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle"  id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        {{auth()->user()->nombre}}
+      </a>
+      <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+        <form method="POST" action="{{ route('logout') }}">
         @csrf
-        <a class="btn" type="button" :href="route('logout')" onclick="event.preventDefault();
+        <a class="dropdown-item" type="button" :href="route('logout')" onclick="event.preventDefault();
                                         this.closest('form').submit();">
           {{ __('Log out') }}
         </a>
       </form>
-      @endif
+      </div>
     </li>
+    @endif
 
 </nav>
+</body>
+</html>
