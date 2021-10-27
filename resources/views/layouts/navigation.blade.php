8be7448f33c36451ab23c39ff3dc7065a62b8a44
santiolaciregui <54482877+santiolaciregui@users.noreply.github.com>
2021-10-27 20:16:25 -0300

    errores solucionados

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,42 +1,4 @@
-<html>
-  <head>
-    
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-  </head>
-  <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-<<<<<<< Updated upstream
-  <ul class="navbar-nav mr-auto">
-    <li class="nav-item active">
-      <a class="nav-link" href="/">Inicio <span class="sr-only">(current)</span></a>
-    </li>
-    @if(auth()->user()==null)
-    <li class="nav-item">
-      <a class="nav-link" href="/login">Log in</a>
-    </li>
-    @else
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle"  id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        {{auth()->user()->nombre}}
-      </a>
-      <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-        <form method="POST" action="{{ route('logout') }}">
-        @csrf
-        <a class="dropdown-item" type="button" :href="route('logout')" onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-          {{ __('Log out') }}
-        </a>
-      </form>
-      </div>
-    </li>
-    @endif
-
-</nav>
-</body>
-</html>
-=======
-  
+<nav class="navbar navbar-expand-lg navbar-light bg-light"> 
   <button class="navbar-toggler" type="button"  data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -54,7 +16,7 @@
       @else
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Usuario
+          {{auth()->user()->nombre}}
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
         @if(Auth::user()->hasRole('cliente') || Auth::user()->hasRole('familiar'))
@@ -76,4 +38,3 @@
     </ul>
   </div>
 </nav>
->>>>>>> Stashed changes
